b47017da18c501206f62aece0f725971f09bc5dc
Matthew Weaber <matthewweaber89@gmail.com>
2020-09-09 18:34:36 -0400

    array methods

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const main = document.getElementById('main')
 const addUserBtn = document.getElementById('add-user')
 const doubleBtn = document.getElementById('double')
-const showMillionairesBtn = document.getElementById('show-millionairs')
+const showMillionairesBtn = document.getElementById('show-millionaires')
 const sortBtn = document.getElementById('sort')
 const calculateBtn = document.getElementById('calculate-wealth')
 
@@ -63,12 +63,22 @@ function sortRichest() {
 }
 
 //show millionaires
-function showMillis() {
-    data.filter(milli => milli > )
+function showMillion() {
+    data = data.filter(user => user.money > 1000000)
+    updateDOM()
+}
+
+function allWealth() {
+    const wealth = data.reduce((total, user) => (total + user.money), 0)
+
+    const wealthEl = document.createElement('div')
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3> `
+    main.appendChild(wealthEl);
 }
 
 //Event listeners
 addUserBtn.addEventListener('click', getRandUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortRichest)
-showMillionairesBtn.addEventListener('click', showMillis)
+showMillionairesBtn.addEventListener('click', showMillion)
+calculateBtn.addEventListener('click', allWealth)
